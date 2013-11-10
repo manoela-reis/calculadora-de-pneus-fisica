c9f5e9e2d8680c52a6b0f78e74365f538b8b98f0
manoelabarreto.nave <manoelabarreto.nave@d8b2edc7-3534-a1cf-c81c-a158cb609985>
2013-11-10 21:33:18 +0000

    Creditos e legislação

---
+++ trunk/OpcaoPneu_Fisica/src/com/example/opcaopneu_fisica/Creditos.java
@@ -0,0 +1,13 @@
+package com.example.opcaopneu_fisica;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Creditos extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.creditos);
+	}
+}
