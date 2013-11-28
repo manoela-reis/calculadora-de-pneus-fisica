2846667bb205178a93ab0cec92dedec1ed93bc47
manoelabarreto.nave <manoelabarreto.nave@d8b2edc7-3534-a1cf-c81c-a158cb609985>
2013-11-28 01:04:28 +0000

    todas as contas feitas e funcionando // faltando exceções

--- trunk/OpcaoPneu_Fisica/src/com/example/opcaopneu_fisica/Desejado.java
+++ trunk/OpcaoPneu_Fisica/src/com/example/opcaopneu_fisica/Desejado.java
@@ -20,63 +20,74 @@ public class Desejado extends Activity {
 	EditText insiravalue;
 	double polegadasDiam;
 	double alturaELarg;
-	double percentAlt;
+	double percent;
 	double somaParcial;
+
+	double polegadasDiam2;
+
+	int newDiam;
+	private double somaParcial2;
+	private double diferençaFinal;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.desejado);
 
-		/*
-		 * if (diametro.isChecked() == true || altura.isChecked() == true ||
-		 * largura.isChecked() == true || velmax.isChecked() == true ||
-		 * cargamax.isChecked() == true) {
-		 * 
-		 * insiravalue.setEnabled(true); insiravalue.setText("");
-		 * 
-		 * if (insiravalue.getText() != null) {
-		 * 
-		 * botao.setClickable(true); botao.setEnabled(true); }
-		 * 
-		 * }
-		 */
 		initialize();
 	}
 
-
 	private void initialize() {
-		
+
 		diametro = (RadioButton) findViewById(R.id.diametroo);
-		botao = (Button) findViewById(R.id.calcular);	
-		insiravalue = (EditText) findViewById(R.id.altura);
+		botao = (Button) findViewById(R.id.calcular);
+		insiravalue = (EditText) findViewById(R.id.valor);
 	}
 
-	public void insira2(View v) {
-		Intent i = new Intent();
-		i.setClass(this, Desejado.class);
-		startActivity(i);
-	}
 	public void diametro(View v) {
 		botao.setEnabled(true);
 	}
+
 	public void calcula(View v) {
-		
-		 polegadasDiam = Calculate.getDiametro() * 25.4;
-		 percentAlt = Calculate.getAltura()/100;
-		 alturaELarg = (Calculate.getLargura() * percentAlt) * 2;
-		 somaParcial = polegadasDiam + alturaELarg;
-		 
-		 //Log.i("oi", Double.toString(somaParcial));
-		 Toast.makeText(getApplicationContext(), Double.toString(percentAlt), Toast.LENGTH_SHORT).show();
-			
-		 
-		/*if(insiravalue.getText() != null){
-			
-			Toast.makeText(getApplicationContext(), Double.toString(somaParcial), Toast.LENGTH_SHORT).show();
-		}*/
-		
-		
+
+		polegadasDiam = Calculate.getDiametro() * 25.4;
+		alturaELarg = ((Calculate.getLargura() * Calculate.getAltura()) / 100) * 2;
+		somaParcial = polegadasDiam + alturaELarg;
+
+		newDiam = Integer.parseInt(insiravalue.getText().toString());
+		polegadasDiam2 = newDiam * 25.4;
+		somaParcial2 = polegadasDiam2 + alturaELarg;
+
+		if (somaParcial >= somaParcial2) {
+
+			diferençaFinal = somaParcial - somaParcial2;
+		}
+		if (somaParcial < somaParcial2) {
+
+			diferençaFinal = somaParcial2 - somaParcial;
+		}
+
+		percent = (diferençaFinal * 100) / somaParcial;
+
+		Toast.makeText(getApplicationContext(),
+				Double.toString(diferençaFinal), Toast.LENGTH_SHORT).show();
+
+		if (percent <= 2) {
+			Toast.makeText(getApplicationContext(), "é possível",
+					Toast.LENGTH_SHORT).show();
+		} else {
+
+			Toast.makeText(getApplicationContext(), "não é possível",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		/*
+		 * if(insiravalue.getText() != null){
+		 * 
+		 * Toast.makeText(getApplicationContext(), Double.toString(somaParcial),
+		 * Toast.LENGTH_SHORT).show(); }
+		 */
+
 	}
 
 }
